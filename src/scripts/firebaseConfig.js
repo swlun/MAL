5cdbf18041240121a8be58a98614048022b5849c
swlun <46320116+swlun@users.noreply.github.com>
2020-02-16 23:53:53 +0800

    Add files via upload

---
+++ src/scripts/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default {
+    apiKey: "AIzaSyBW5IGDtjH-fu2J9tgH3hK08zsiBYM3Dvs",
+    authDomain: "mal-db.firebaseapp.com",
+    databaseURL: "https://mal-db.firebaseio.com",
+    projectId: "mal-db",
+    storageBucket: "mal-db.appspot.com",
+    messagingSenderId: "982184642166",
+    appId: "1:982184642166:web:9ac91339d90f7dcf35c157",
+    measurementId: "G-DXRFCZM31G"
+}
---
+++ src/views/Dashboard.vue
@@ -0,0 +1,38 @@
+<template>
+    <div>
+        <h1>Dashboard</h1>
+        <h3>View</h3>
+        <ul>
+            <li><h3>Users</h3></li>
+            <li v-for="user in users" v-bind:key="user.Id">
+                {{ user.UserName }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import db from '../scripts/firebaseInit'
+
+export default {
+    name: 'dashboard',
+    data() {
+        return {
+            users: []
+        }
+    },
+    created() {
+        db.collection('users').get().then(querySnapshot => [
+            querySnapshot.forEach(doc => {
+                const data = {
+                    'Id': doc.id,
+                    'UserId': doc.data().Id,
+                    'UserName': doc.data().Name
+                }
+                console.log(data);
+                this.users.push(data);
+            })
+        ])
+    }
+}
+</script>
